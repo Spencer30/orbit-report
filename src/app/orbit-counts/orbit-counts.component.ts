9918766972b25155ccb1803415e8c20f23eb31b1
Spencer Lyons <sjl8r8@gmail.com>
2019-12-09 20:21:29 -0600

    completed bonus part B - Count Satellites

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -14,16 +14,66 @@ export class OrbitCountsComponent implements OnInit {
   ngOnInit() {
   }
 
-  countType() {
-    console.log(this.satellites[5].type);
-    console.log(this.satellites[5].type.toLowerCase());
+  countDebris() {
     let debrisArr = [];
-    for (let i=0; i<this.satellites.length; i++){
+    for (let i = 0; i < this.satellites.length; i++) {
       if (this.satellites[i].type.toLowerCase() === 'space debris') {
         debrisArr.push(this.satellites[i].type);
+      }
     }
+    return debrisArr.length
   }
-  return debrisArr.length
-}
+
+  countComm() {
+    let comm = [];
+    for (let i = 0; i < this.satellites.length; i++) {
+      if (this.satellites[i].type.toLowerCase() === 'communication') {
+        comm.push(this.satellites[i].type);
+      }
+    }
+    return comm.length
+  }
+
+  countProbe() {
+    let probe = [];
+    for (let i = 0; i < this.satellites.length; i++) {
+      if (this.satellites[i].type.toLowerCase() === 'probe') {
+        probe.push(this.satellites[i].type);
+      }
+    }
+    return probe.length
+  }
+
+  countPosition() {
+    let position = [];
+    for (let i = 0; i < this.satellites.length; i++) {
+      if (this.satellites[i].type.toLowerCase() === 'positioning') {
+        position.push(this.satellites[i].type);
+      }
+    }
+    return position.length
+  }
+
+  countSS() {
+    let spaceStat = [];
+    for (let i = 0; i < this.satellites.length; i++) {
+      if (this.satellites[i].type.toLowerCase() === 'space station') {
+        spaceStat.push(this.satellites[i].type);
+      }
+    }
+    return spaceStat.length
+  }
+
+  countTele() {
+    let tele = [];
+    for (let i = 0; i < this.satellites.length; i++) {
+      if (this.satellites[i].type.toLowerCase() === 'telescope') {
+        tele.push(this.satellites[i].type);
+      }
+    }
+    return tele.length
+  }
+
+
 
 }
